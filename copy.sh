932cffc8fd9e1acb640e924ec809e6074712dfb3
nsn90255 <nsn90255@gmail.com>
2025-03-26 18:10:06 +0100

    format hdd

--- copy.sh
+++ copy.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
+set -e
+
+if ![ "$(id -u)" -eq 0 ]; then
+	echo "run as root."
+	return 1
 
 TARGET_DISK="/dev/sda"
 
+# wipe partitions and create a new one with mbr
+echo -e "o\nw" | sudo fdisk ${TARGET_DISK}1
+
+echo -e "o\nn\np\n1\n\nt\n83\nw" | sudo fdisk ${TARGET_DISK}1
+
+mkfs.ext4 ${TARGET_DISK}1
+
 mount ${TARGET_DISK}1 /mnt
 
+# copy the live system over to the new partition
 rsync -aAXv / --exclude=/mnt --exclude=/proc --exclude=/sys --exclude=/dev /mnt/
 
+# set up grub
 grub-install --root-directory=/mnt ${TARGET_DISK}
 
 chroot /mnt grub-mkconfig -o /mnt/boot/grub/grub.cfg
