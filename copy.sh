2c4e0b84fa3f1484613592edf9eb9e5c0da0fb58
nsn90255 <nsn90255@gmail.com>
2025-03-26 18:22:49 +0100

    add echo statements

--- copy.sh
+++ copy.sh
@@ -11,19 +11,36 @@ TARGET_DISK="/dev/sda"
 # wipe partitions and create a new one with mbr
 echo -e "o\nw" | sudo fdisk ${TARGET_DISK}
 
+echo "disk wiped"
+
 echo -e "o\nn\np\n1\n\nt\n83\nw" | sudo fdisk ${TARGET_DISK}
+
+echo "partition created"
 
 mkfs.ext4 ${TARGET_DISK}1
 
+echo "installed filesystem on partition"
+
 mount ${TARGET_DISK}1 /mnt
+
+echo "mounted partition to mnt"
 
 # copy the live system over to the new partition
 rsync -aAXv / --exclude=/mnt --exclude=/proc --exclude=/sys --exclude=/dev /mnt/
 
+echo "copied rootfs to partition"
+
 # set up grub
 grub-install --root-directory=/mnt ${TARGET_DISK}
 
+echo "installed grub on partition"
+
 chroot /mnt grub-mkconfig -o /mnt/boot/grub/grub.cfg
 
+echo "created grub config"
+
 umount /mnt
+
+echo "unmounted partition"
+
 echo "locked and loaded"
